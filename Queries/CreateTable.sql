fa8960b26c2267daf4cf63248ce15701ce168b6c
JGMelon22 <jgvasconcellos22@gmail.com>
2022-04-15 13:03:19 -0300

    Revisando a criação de tabelas com "Create table"

---
+++ Queries/CreateTable.sql
@@ -0,0 +1,35 @@
+/* CREATE TABLE */ 
+
+-- CRIANDO O BANCO DE DADOS 
+CREATE DATABASE YoutubeLite;
+
+-- MUDANDO PARA A BAASE NOVA
+USE YoutubeLite;
+
+-- CRIANDO AS TABELAS
+BEGIN TRANSACTION
+CREATE TABLE Canal
+(
+    CanalId INT PRIMARY KEY,
+    Nome VARCHAR(150) NOT NULL,
+    ContagemInscritos INT DEFAULT 0,
+    DataCriacao DATETIME NOT NULL
+);
+COMMIT -- ROLLBACK;
+GO
+
+BEGIN TRANSACTION
+CREATE TABLE Video
+(
+    VideoId INT PRIMARY KEY,
+    Nome VARCHAR(150),
+    Vizualizacoes INT DEFAULT 0,
+    Likes INT DEFAULT 0,
+    Dislikes INT DEFAULT 0,
+    Duracao INT NOT NULL,
+    CanalId INT,
+    CONSTRAINT FK_CanalId FOREIGN KEY(CanalId)
+        REFERENCES Canal(CanalId)
+);
+COMMIT -- ROLLBACK;
+GO
